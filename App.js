82dc4b0ff4922818e2ff2ab7659466f9d863b51e
skim546 <157713541+skim546@users.noreply.github.com>
2024-06-23 21:06:49 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './screens/Login';
+import Register from './screens/Register';
+import ChatBot from './screens/ChatBot';
+import StickyNotesMap from './screens/StickyNotesMap';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="ChatBot" component={ChatBot} />
+        <Stack.Screen name="StickyNotesMap" component={StickyNotesMap} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
